d5dab321d02f194275d889286975b92bf5a56bb5
Ayokunle <therealayokunle@gmail.com>
2020-01-26 14:50:53 +0100

    Updates

---
+++ precache-manifest.f2d93632a12c4c6e721c581f911753c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff815b43f59fc02bf94c4d934f7a9c78",
+    "url": "/pizzarecipe/index.html"
+  },
+  {
+    "revision": "4d6e6544fc70a9a03620",
+    "url": "/pizzarecipe/static/css/main.6b284299.chunk.css"
+  },
+  {
+    "revision": "d27ba6dadf7b6d6d23f2",
+    "url": "/pizzarecipe/static/js/2.aa9b191f.chunk.js"
+  },
+  {
+    "revision": "4d6e6544fc70a9a03620",
+    "url": "/pizzarecipe/static/js/main.305851f9.chunk.js"
+  },
+  {
+    "revision": "930e17a6484d979a072b",
+    "url": "/pizzarecipe/static/js/runtime~main.a06300e1.js"
+  }
+]);
